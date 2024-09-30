4e0320580a0b42ecee2842ca3e09dc1e9dd44d11
Luciano Sena <lucianosena21335@gmail.com>
2024-09-30 19:52:12 -0300

    Adicionei O Departamento das entidades das classes

--- Semana-03/Projeto_JDBC/src/application/Program.java
+++ Semana-03/Projeto_JDBC/src/application/Program.java
@@ -1,39 +1,19 @@
 package application;
+
 import db.DB;
+import model.entities.Department;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 
-
 public class Program {
     public static void main(String[] args) {
 
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            conn = DB.getConnection();
-
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("Select * from department");
-
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-
-            }
-
-        }
-        catch(SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            DB.closeResultSet(rs);
-            DB.closeStatment(stmt);
-            DB.closeConnection();
-        }
-
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 
 }
